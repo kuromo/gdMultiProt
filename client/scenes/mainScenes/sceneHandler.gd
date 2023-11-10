58468a6f29999cce6989c1c8850547d42c1bcef6
kuromo <michael.ochsner@hotmail.com>
2023-11-10 21:41:42 +0100

    load tilemap on client start

--- client/scenes/mainScenes/sceneHandler.gd
+++ client/scenes/mainScenes/sceneHandler.gd
@@ -1,8 +1,8 @@
 extends Node
 
-#var map = preload("res://scenes/mainScenes/map.tscn")
+var map = preload("res://scenes/mainScenes/map.tscn")
 
 func _ready():
-#	var mapInstance = map.instance()
-#	add_child(mapInstance)
+	var mapInstance = map.instantiate()
+	add_child(mapInstance)
 	pass
