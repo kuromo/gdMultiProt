abebb2515eb4248e3838adc1a5eb5d5fbf4f9f2d
kuromo <michael.ochsner@hotmail.com>
2023-11-25 13:26:21 +0100

    extrapolate player movement
    extrapolate player movement if there are no future but two past states

--- client/scenes/mainScenes/map.gd
+++ client/scenes/mainScenes/map.gd
@@ -6,7 +6,7 @@ extends Node2D
 var playerTemplate = preload("res://scenes/entityScenes/playerTemplate.tscn")
 
 var lastWorldState = 0
-var worldStateBuffer = []
+var worldStateBuffer = [] # states: [pastPast, past, future, ..anyFurtherFuture]
 var interpolationOffset = 0.1
 
 # Called when the node enters the scene tree for the first time.
@@ -52,6 +52,8 @@ func spawnPlayer(usrId, spawnPos):
 func despawnPlayer(usrId):
 	print("despawn player:")
 	print(usrId)
+	# TODO maybe add logout timer on server https://youtu.be/XGyrKmOxLcc?si=rOgmwTxE2Ljm3PDm&t=659
+	await get_tree().create_timer(0.2)
 	%otherPlayers.get_node(str(usrId)).queue_free()
 
 
@@ -64,18 +66,32 @@ func updateWorldState(worldState):
 func _physics_process(delta):
 	var renderTime = Time.get_unix_time_from_system() - interpolationOffset
 	if worldStateBuffer.size() > 1:
-		while worldStateBuffer.size() > 2 and renderTime > worldStateBuffer[1]["T"]:
+		while worldStateBuffer.size() > 2 and renderTime > worldStateBuffer[2]["T"]:
 			worldStateBuffer.remove_at(0)
-		var interpolationFactor = (renderTime - worldStateBuffer[0]["T"]) / (worldStateBuffer[1]["T"] - worldStateBuffer[0]["T"])
-		for player in worldStateBuffer[1].keys():
-			if str(player) == "T":
-				continue
-			if player == multiplayer.get_unique_id():
-				continue
-			if !worldStateBuffer[0].has(player):
-				continue
-			if %otherPlayers.has_node(str(player)):
-				var newPos = lerp(worldStateBuffer[0][player]["P"], worldStateBuffer[1][player]["P"], interpolationFactor)
-				%otherPlayers.get_node(str(player)).movePlayer(newPos)
-			else:
-				spawnPlayer(player, worldStateBuffer[1][player]["P"])
+		if worldStateBuffer.size() > 2: # we have a future state
+			var interpolationFactor = (renderTime - worldStateBuffer[1]["T"]) / (worldStateBuffer[2]["T"] - worldStateBuffer[1]["T"])
+			for player in worldStateBuffer[2].keys():
+				if str(player) == "T":
+					continue
+				if player == multiplayer.get_unique_id():
+					continue
+				if !worldStateBuffer[1].has(player):
+					continue
+				if %otherPlayers.has_node(str(player)):
+					var newPos = lerp(worldStateBuffer[1][player]["P"], worldStateBuffer[2][player]["P"], interpolationFactor)
+					%otherPlayers.get_node(str(player)).movePlayer(newPos)
+				else:
+					spawnPlayer(player, worldStateBuffer[2][player]["P"])
+		elif renderTime > worldStateBuffer[1]["T"]: # no future state
+			var extrapolationFactor = (renderTime - worldStateBuffer[0]["T"]) / (worldStateBuffer[1]["T"] - worldStateBuffer[0]["T"]) - 1.0
+			for player in worldStateBuffer[1].keys():
+				if str(player) == "T":
+					continue
+				if player == multiplayer.get_unique_id():
+					continue
+				if !worldStateBuffer[0].has(player):
+					continue
+				if %otherPlayers.has_node(str(player)):
+					var posDelta = worldStateBuffer[1][player]["P"] - worldStateBuffer[0][player]["P"]
+					var newPos = worldStateBuffer[1][player]["P"] + (posDelta * extrapolationFactor)
+					%otherPlayers.get_node(str(player)).movePlayer(newPos)
